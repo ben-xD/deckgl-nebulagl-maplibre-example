40cc643ee70ab2a8b05a29ea554573cd4b15c837
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-02-15 16:17:22 +0000

    this allows tapCount==2 when double clicking selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import { Map } from "maplibre-gl";
-import { MapboxOverlay } from "@deck.gl/mapbox/typed";
-// import type { Layer } from "@deck.gl/core/typed";
+import { MapboxLayer, MapboxOverlay } from "@deck.gl/mapbox/typed";
 import { EditableGeoJsonLayer } from "@nebula.gl/layers";
-import { DrawPolygonMode } from "@nebula.gl/edit-modes";
 import type { FeatureCollection } from "geojson";
+import { TranslateMode } from "@nebula.gl/edit-modes";
 // Started from https://github.com/visgl/deck.gl/blob/8.9-release/examples/get-started/pure-js/mapbox/app.js
 // MapboxOverlay vs MapboxLayer? Layer is just a layer. Overlay
 
@@ -14,7 +13,7 @@ import type { FeatureCollection } from "geojson";
 
 function App() {
   const mapContainer = useRef<HTMLDivElement>(null);
-
+  const mapboxLayerRef = useRef<MapboxLayer<EditableGeoJsonLayer>>(null);
   const [selectedFeatureIndexes, setSelectedFeatureIndexes] = useState<
     number[]
   >([]);
@@ -59,64 +58,92 @@ function App() {
     // editableGeojsonLayerInMapboxLayer.type = EditableGeoJsonLayer;
 
     // adapted from https://nebula.gl/docs/api-reference/layers/editable-geojson-layer
-    const editableGeoJsonLayer = new EditableGeoJsonLayer({
-      id: "editable-geojson-layer-1",
+    // Put the props directly inside MapboxLayer instead of EditableGeojsonLayer. map.addLayer(mapboxLayer). Editing modes worked here.
+    const maplibreLayer = new EditableGeoJsonLayer({
+      opacity: 0.1,
+      id: "maplibre-layer-wrapping-editable-geojson-layer",
       data: featureCollection,
       pickable: true,
-      selectedFeatureIndexes,
-      mode: new DrawPolygonMode(),
+      // type is missing but this is important
+      type: EditableGeoJsonLayer,
+      selectedFeatureIndexes: [],
+      mode: TranslateMode,
       onClick: (pickInfo, hammerInput) => {
+        setSelectedFeatureIndexes([pickInfo.index]);
         console.log("click", { pickInfo, hammerInput });
       },
-      onEdit: (updatedData, editType, featureIndexes, editContext) => {
-        console.log("onEdit called", { editType, featureIndexes, editContext });
-        setFeatureCollection(updatedData);
+      onEdit: (
+        updatedData: FeatureCollection,
+        editType: string,
+        featureIndexes: number[],
+        editContext: any | null
+      ) => {
+        if (updatedData && updatedData.features) {
+          console.log("onEdit called with features", {
+            updatedData,
+            editType,
+            featureIndexes,
+            editContext,
+          });
+          setFeatureCollection(updatedData);
+        } else {
+          console.error("onEdit called with no features", {
+            updatedData,
+            editType,
+            featureIndexes,
+            editContext,
+          });
+        }
       },
     });
-    // docs: https://deck.gl/docs/api-reference/mapbox/mapbox-overlay
-    const deckOverlay = new MapboxOverlay({
-      layers: [
-        editableGeoJsonLayer,
-        // new GeoJsonLayer({
-        //   id: "airports",
-        //   data: AIRPORTS,
-        //   // Styles
-        //   filled: true,
-        //   pointRadiusMinPixels: 2,
-        //   pointRadiusScale: 2000,
-        //   getPointRadius: (f) => 11 - f?.properties?.scalerank,
-        //   getFillColor: [200, 0, 80, 180],
-        //   // Interactive props
-        //   pickable: true,
-        //   autoHighlight: true,
-        //   onClick: (info) =>
-        //     // eslint-disable-next-line
-        //     info.object &&
-        //     alert(
-        //       `${info.object.properties.name} (${info.object.properties.abbrev})`
-        //     ),
-        // }),
-        // new ArcLayer({
-        //   id: "arcs",
-        //   data: AIR_PORTS,
-        //   dataTransform: (d) =>
-        //     d.features.filter((f) => f.properties.scalerank < 4),
-        //   // Styles
-        //   getSourcePosition: (f) => [-0.4531566, 51.4709959], // London
-        //   getTargetPosition: (f) => f.geometry.coordinates,
-        //   getSourceColor: [0, 128, 200],
-        //   getTargetColor: [200, 0, 80],
-        //   getWidth: 1,
-        // }),
-      ],
+    const overlayControl = new MapboxOverlay({
+      layers: [maplibreLayer],
     });
+    mapboxLayerRef.current = overlayControl;
+    // docs: https://deck.gl/docs/api-reference/mapbox/mapbox-overlay
+    // const deckOverlay = new MapboxOverlay({
+    //   layers: [
+    //     editableGeoJsonLayer,
+    //     // new GeoJsonLayer({
+    //     //   id: "airports",
+    //     //   data: AIRPORTS,
+    //     //   // Styles
+    //     //   filled: true,
+    //     //   pointRadiusMinPixels: 2,
+    //     //   pointRadiusScale: 2000,
+    //     //   getPointRadius: (f) => 11 - f?.properties?.scalerank,
+    //     //   getFillColor: [200, 0, 80, 180],
+    //     //   // Interactive props
+    //     //   pickable: true,
+    //     //   autoHighlight: true,
+    //     //   onClick: (info) =>
+    //     //     // eslint-disable-next-line
+    //     //     info.object &&
+    //     //     alert(
+    //     //       `${info.object.properties.name} (${info.object.properties.abbrev})`
+    //     //     ),
+    //     // }),
+    //     // new ArcLayer({
+    //     //   id: "arcs",
+    //     //   data: AIR_PORTS,
+    //     //   dataTransform: (d) =>
+    //     //     d.features.filter((f) => f.properties.scalerank < 4),
+    //     //   // Styles
+    //     //   getSourcePosition: (f) => [-0.4531566, 51.4709959], // London
+    //     //   getTargetPosition: (f) => f.geometry.coordinates,
+    //     //   getSourceColor: [0, 128, 200],
+    //     //   getTargetColor: [200, 0, 80],
+    //     //   getWidth: 1,
+    //     // }),
+    //   ],
+    // });
     // We can also setProps later
     // deckOverlay.setProps({ layers: [editableGeoJsonLayer] });
 
     // Interesting alternative: await map.once('load');
     map.on("load", () => {
       // maplibre (Map#addControl's parameter IControl) and deck.gl (MapboxOverlay) don't 100% agree
-      map.addControl(deckOverlay);
+      map.addControl(overlayControl);
       // How to add EditableGeojsonLayer to maplibre? Can't use the addLayer anymore.
       // map.addLayer();
       // Use addControl and MapboxOverlay, following https://maplibre.org/maplibre-gl-js/docs/examples/add-deckgl-layer-using-rest-api/
@@ -133,6 +160,25 @@ function App() {
       map.remove();
     };
   }, []);
+
+  useEffect(() => {
+    mapboxLayerRef.current!.setProps({
+      id: "maplibre-layer-wrapping-editable-geojson-layer",
+      selectedFeatureIndexes,
+    });
+  }, [selectedFeatureIndexes]);
+
+  useEffect(() => {
+    if (featureCollection) {
+      console.log(
+        `feature changes. ${featureCollection?.features?.length} features`
+      );
+      mapboxLayerRef.current!.setProps({
+        id: "maplibre-layer-wrapping-editable-geojson-layer",
+        data: featureCollection,
+      });
+    }
+  }, [featureCollection]);
 
   return (
     <>
